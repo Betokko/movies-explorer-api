5a4cdf3a6cb02f8e8c7f73a146ed10f4cd594c34
Artur Nikitin <branewor@gmail.com>
2022-07-21 15:25:25 +0400

    feat: create login controller

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 // глобальные импорты
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
 
 // локальные импорты
 const userRouter = require('./routes/user');
 const movieRouter = require('./routes/movie');
+const authRouter = require('./routes/auth');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
+app.use('/', authRouter);
 app.use('/users', userRouter);
 app.use('/movies', movieRouter);
+app.use(errors());
 
 mongoose.connect(
   'mongodb+srv://admin:admin@cluster0.zzbidms.mongodb.net/?retryWrites=true&w=majority',
---
+++ controllers/login.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+const UnauthorizedError = require('../error-classes/UnauthorizedError');
+
+const JWT_SECRET = process.env.JWT_SECRET || 'pee-pee-poo-poo';
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  return User.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        next(new UnauthorizedError('Пользователь с таким email не найден'));
+      } else {
+        bcrypt.compare(password, user.password, (error, result) => {
+          if (result) {
+            const token = jwt.sign({ _id: user._id }, JWT_SECRET);
+            return res.status(200).send({ token });
+          }
+          next(new UnauthorizedError('Введен неверный email или password'));
+          return null;
+        });
+      }
+    });
+};
+module.exports = login;
