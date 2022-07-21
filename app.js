d32ca69feca9f9ef4aeb08c15f3a8c33e67dc312
Artur Nikitin <branewor@gmail.com>
2022-07-21 20:21:29 +0400

    feat: create centralizeErrorHandler

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 // глобальные импорты
 const express = require('express');
 const mongoose = require('mongoose');
@@ -10,8 +12,13 @@ const movieRouter = require('./routes/movie');
 const authRouter = require('./routes/auth');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const NotFoundError = require('./error-classes/NotFoundError');
+const centralizeErrorHandler = require('./middlewares/centralize-error-handler');
 
+// переменные окружения
 const PORT = process.env.PORT || 3000;
+const DB_CONN = process.env.DB_CONN
+  || 'mongodb+srv://admin:admin@cluster0.zzbidms.mongodb.net/?retryWrites=true&w=majority'; // резервания облачная БД
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,11 +27,11 @@ app.use('/', authRouter);
 app.use(auth); // защита роутов авторизацией
 app.use('/users', userRouter);
 app.use('/movies', movieRouter);
+app.use('*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
 app.use(errorLogger); // логгер ошибок
 app.use(errors()); // обработчик ошибок celebrate
+app.use(centralizeErrorHandler); // централизованный обработчик ошибок
 
-mongoose.connect(
-  'mongodb+srv://admin:admin@cluster0.zzbidms.mongodb.net/?retryWrites=true&w=majority',
-);
+mongoose.connect(DB_CONN);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
