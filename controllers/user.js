07156b8baf7a371c2f9491fa5e499097338c2167
Artur Nikitin <branewor@gmail.com>
2022-07-21 19:56:31 +0400

    refact: user controller

--- controllers/user.js
+++ controllers/user.js
@@ -3,19 +3,47 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const BadRequestError = require('../error-classes/BadRequestError');
 const ConflictError = require('../error-classes/ConflictError');
+const NotFoundError = require('../error-classes/NotFoundError');
 
-const getUser = (req, res) => {
-  res.json('getUser');
+const getUser = (req, res, next) => {
+  User.findById(req.params.id).then((user) => {
+    if (!user) {
+      return next(new NotFoundError('Пользователь не найден'));
+    }
+    return res.send(user);
+  });
 };
 
-const updateUser = (req, res) => {
-  res.json('updateUser');
+const updateUser = (req, res, next) => {
+  const { email, name } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { email, name },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (!user) {
+        next(new NotFoundError('Пользователь не найден'));
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Некорректные данные при создании карточки'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const createUser = (req, res, next) => {
   const { email, password, name } = req.body;
   if (!email || !password || !name) {
-    next(new BadRequestError('Поля: name, email или password не могут быть пустыми!'));
+    next(
+      new BadRequestError(
+        'Поля: name, email или password не могут быть пустыми!',
+      ),
+    );
   }
   return User.findOne({ email }).then((user) => {
     if (user) {
@@ -27,7 +55,11 @@ const createUser = (req, res, next) => {
         .then((response) => res.status(200).send({ email: response.email, name: response.name }))
         .catch((err) => {
           if (err.name === 'ValidationError') {
-            next(new BadRequestError('Некорректные данные при создании пользователя'));
+            next(
+              new BadRequestError(
+                'Некорректные данные при создании пользователя',
+              ),
+            );
           } else {
             next(err);
           }
